06666ad93397d75d528885da44c27b060b946abe
Criscrav15 <cristiancravero023@gmail.com>
2023-04-13 17:13:14 -0300

    subo back para empezar pipeline

---
+++ back/src/main/java/Integrador5/demo/security/WebSecurityConfig.java
@@ -0,0 +1,85 @@
+package Integrador5.demo.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@AllArgsConstructor
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+        private final UserDetailServiceImpl userDetailService;
+        private final JWTauthorizationFilter jwtAuthorizationFilter;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173", "http://grupo5-c2.s3-website.us-east-2.amazonaws.com")
+                .allowCredentials(true)
+                .allowedMethods("*");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        JWTathenticationFilter jwtAthenticationFilter = new JWTathenticationFilter();
+        jwtAthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAthenticationFilter.setFilterProcessesUrl("/login");
+
+        return http
+                .csrf().disable().cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/ciudad/**","/reserva/listarPorProducto/**","/usuario/**","/authenticate/**" ,"/caracteristica/**", "/categoria/**", "/imagen/**", "/producto/**").permitAll()
+                .requestMatchers("/producto/crearProducto").hasAuthority("admin")
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(jwtAthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+
+
+}
